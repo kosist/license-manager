fb7aa08b33f727e379bf352df125e9ece0f2de4b
Ivan <kosistivan@gmail.com>
2020-11-08 00:37:34 +0100

    Added view for customers (just as a list)

--- DAL/MockRepositories/MockCustomerRepository.cs
+++ DAL/MockRepositories/MockCustomerRepository.cs
@@ -5,6 +5,23 @@ namespace DAL.MockRepositories
 {
     public class MockCustomerRepository : GenericMockRepository<Customer>, ICustomerRepository
     {
-        
+        public MockCustomerRepository() : base()
+        {
+            CreateCustomersList();
+        }
+
+        private void CreateCustomersList()
+        {
+            var customer1 = new Customer
+            {
+                Id = 0,
+                Address = "Dummy address",
+                Country = "Dummy country",
+                Name = "Dummy customer"
+            };
+
+            Insert(customer1);
+        }
+
     }
 }
--- LicenseManagerWeb/Controllers/CustomersController.cs
+++ LicenseManagerWeb/Controllers/CustomersController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAL.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LicenseManagerWeb.Controllers
 {
     public class CustomersController : Controller
     {
+        private ICustomerRepository _customerRepo;
+
+        public CustomersController(ICustomerRepository customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_customerRepo.GetList());
         }
     }
 }
