96105fb8f71f91289bdfc3836bd975916af862ef
Ivan <kosistivan@gmail.com>
2020-11-08 13:00:00 +0100

    Added generic and mock repository for product

---
+++ DAL/MockRepositories/MockProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Repositories;
+using Domain;
+
+namespace DAL.MockRepositories
+{
+    public class MockProductRepository : GenericMockRepository<Product>, IProductRepository
+    {
+        public MockProductRepository() : base()
+        {
+            CreateProductsList();
+        }
+
+        private void CreateProductsList()
+        {
+            var product = new Product
+            {
+                Id = 0,
+                Name = "Dummy SW Product 00",
+                Version = "Version 00.00",
+            };
+            Insert(product);
+        }
+    }
+}
--- Domain/Product.cs
+++ Domain/Product.cs
@@ -5,6 +5,8 @@ namespace Domain
     public class Product : BaseItem
     {
         public int ProductId { get; set; }
+        public string Name { get; set; }
+        public License License { get; set; }
         public string Version { get; set; }
         public ProductMeta ProductMetadata { get; set; }
         public List<ProductChange> ProductChanges { get; set; }
