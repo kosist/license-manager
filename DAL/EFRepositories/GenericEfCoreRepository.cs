8f1bdca17a42c9a991e20e591dbb4d87d66b4ad2
Ivan Yakushchenko <ivan.yakushchenko@anv-tech.com>
2020-12-18 14:31:23 +0100

    Added generic class and interface for EfCore

---
+++ DAL/EFRepositories/GenericEfCoreRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace DAL.EFRepositories
+{
+    public abstract class GenericEfCoreRepository<TDomain, TContext> : IEfCoreRepository<TDomain, TContext>
+        where TDomain : BaseItem
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public GenericEfCoreRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int? id)
+        {
+            var entity = _context.Set<TDomain>().Find(id);
+            if (entity == null)
+            _context.Set<TDomain>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public TDomain GetById(int? id)
+        {
+            return _context.Set<TDomain>().Find(id);
+        }
+
+        public IEnumerable<TDomain> GetList()
+        {
+            return _context.Set<TDomain>().ToListAsync().Result;
+        }
+
+        public void Insert(TDomain item)
+        {
+            _context.Set<TDomain>().Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Update(TDomain item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ DAL/EFRepositories/IEfCoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace DAL.EFRepositories
+{
+    public interface IEfCoreRepository<TDomain, TContext> : IGenericItemRepository<TDomain>
+        where TDomain   : BaseItem
+        where TContext  : DbContext
+    {
+
+    }
+}
